4951083620bb07c63a3b863d62aae6e2c0c4f8c5
sandhiya <sandhyasoneta12@gmail.com>
2018-11-05 14:11:22 -0500

    pending

--- app.js
+++ app.js
@@ -2,3 +2,37 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the UFO data from data.js
+console.log(data);
+
+data.forEach((UFOdata) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOdata).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+
+
+  // Assign the data from `data.js` to a descriptive variable
+var date = data;
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+submit.on("click", function() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+     // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    var filteredData = date.filter(dates => dates.datetime === inputValue);
+    console.log(filteredData);
+
+});
+
